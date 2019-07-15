831da0a7c4c2a4e4d72cf6fe24412455fc51542f
Le H Ngo <seiji@o2.pl>
2019-07-15 17:33:07 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  function myFunction(text) {
+    document.querySelector("#tasks").appendChild(document.createElement("li")).innerText = text
+  }
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    input = document.querySelector("#new-task-description").value;
+    myFunction(input);
+  });
+
+
 });
