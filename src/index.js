6836a88b91ae02a99207e6c3bd3245e0e6e172d7
Amy Quinn <amyequinn@hotmail.co.uk>
2019-07-15 18:20:26 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+document.addEventListener("DOMContentLoaded", () =>{
+  const form = document.getElementById('create-task-form')
+  form.addEventListener("submit", addNewTask)
 });
+
+const addNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
